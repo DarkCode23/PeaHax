78146d746dbf59afc748707f824ca1e27b2212e5
DarkCode23 <Visalgaming09@icloud.com>
2022-01-02 15:07:09 +0700

    update

---
+++ js/demo.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+	$('body').on('click', '.demo__ico', function() {
+		$('.demo').toggleClass('demo_open');
+	});
+
+	$('body').on('click', '.demo__theme', function() {
+		$('.demo__theme').removeClass('demo__theme_active');
+		$(this).addClass('demo__theme_active');
+
+		$('body').attr('class', $(this).data('theme'));
+	})
+});
